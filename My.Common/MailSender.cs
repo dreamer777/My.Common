a0e08deb1f3ca88d50e17d21ee3b493a41fceb2d
Konstantin Chernyaev <Konstantin.Chernyaev@uniconf.ru>
2012-11-30 17:00:38 +0400

    email size is limited!

--- My.Common/MailSender.cs
+++ My.Common/MailSender.cs
@@ -23,16 +23,18 @@ namespace My.Common
     public class MailSender
     {
         static readonly Logger Logger = LogManager.GetLogger("MailSender");
+        const int EmailSizeLimit = 15000000;
 
 
         /// <param name="attachments"> collection of ( (file name, file extension), body ) </param>
         /// <param name="messageBody"> html </param>
+        /// <param name="sendIfBigFiles"> If files total size more then allowed limit, so send mail but not all files - only several from them </param>
         /// <exception cref="SmtpFailedRecipientsException"></exception>
         /// <exception cref="SmtpException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
         /// <returns> returns error desc </returns>
         public string SendMessage([NotNull] string mailFrom, [NotNull] string mailTo, [NotNull] string messageBody, [NotNull] string messageTitle,
-                                  IEnumerable<KeyValuePair<Tuple<string, string>, byte[]>> attachments = null)
+                                  List<KeyValuePair<Tuple<string, string>, byte[]>> attachments = null, bool sendIfBigFiles = true)
         {
             int emailId = GetEmailId(mailFrom, mailTo, messageBody, messageTitle);
 
@@ -65,14 +67,27 @@ namespace My.Common
                 SmtpClient smtp = new SmtpClient();
 
                 if (attachments != null)
-                    foreach (KeyValuePair<Tuple<string, string>, byte[]> a in attachments.Where(x => x.Key != null && x.Value != null && x.Value.Length > 0))
+                {
+                    int totalSize = 0;
+                    KeyValuePair<Tuple<string, string>, byte[]>[] ats = attachments
+                            .Where(x => x.Key != null && x.Value != null && x.Value.Length > 0)
+                            .TakeWhile(a => (totalSize += a.Value.Length) < EmailSizeLimit - objEmail.Body.Length*2 - objEmail.Subject.Length*2)
+                            .ToArray();
+                    if (ats.Length < attachments.Count && !sendIfBigFiles)
+                        throw new Exception("files size too big");
+
+                    Logger.Info("{0} attachmets, total size {1}; was {2}, total size {3}", ats.Count(), ats.Sum(a => a.Value.Length),
+                                attachments.Count, attachments.Sum(a => a.Value.Length));
+                    foreach (KeyValuePair<Tuple<string, string>, byte[]> a in ats)
                         objEmail.Attachments.Add(AttachmentHelper.CreateAttachment(new MemoryStream(a.Value),
                                                                                    a.Key.Item1.EndsWith(a.Key.Item2) ? a.Key.Item1
-                                                                                           : (a.Key.Item1 + (a.Key.Item2.StartsWith(".") ? "" : ".") + a.Key.Item2)));
-
+                                                                                           : (a.Key.Item1 + (a.Key.Item2.StartsWith(".") ? "" : ".")
+                                                                                              + a.Key.Item2)));
+                }
                 try
                 {
                     smtp.Send(objEmail);
+                    Logger.Trace("sent ok");
                 }
                 catch (Exception ex)
                 {
